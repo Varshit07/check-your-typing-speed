6a46dbe209f9d4ea13e149d657776fe4622db4a0
Varshit07 <varshitshah97@gmail.com>
2020-09-27 22:10:48 -0700

    added useState hook to store text typed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [textTyped, setTextTyped] = useState("");
+
+  function handleOnChange(event) {
+    setTextTyped(event.target.value);
+  }
+
+
   return (
     <div className="App">
       <h1>Check your typing speed</h1>
-      <textarea rows = "8" cols="60" placeholder="Click Start button to start typing" />
+      <textarea rows = "8" cols="60" placeholder="Click Start button to start typing" value={textTyped} onChange={handleOnChange}/>
       <h2>Time Remaining: ---</h2>
       <button>Start</button>
       <h2>Words Typed: ---</h2>
