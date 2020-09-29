66d9ceab30ef63bb204f3c6174c39f29cc6f6d05
Varshit07 <varshitshah97@gmail.com>
2020-09-29 02:38:23 -0700

    Updates

---
+++ precache-manifest.bbe41212422a022e874f698da38ce289.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d268f971cdb7f3b463ce3e1052bf7a8",
+    "url": "/check-your-typing-speed/index.html"
+  },
+  {
+    "revision": "d19aa00de1d1d1f26dae",
+    "url": "/check-your-typing-speed/static/css/main.d3d38146.chunk.css"
+  },
+  {
+    "revision": "8db47a8ffd17f37bdbd6",
+    "url": "/check-your-typing-speed/static/js/2.b9af3ef9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/check-your-typing-speed/static/js/2.b9af3ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d19aa00de1d1d1f26dae",
+    "url": "/check-your-typing-speed/static/js/main.3cbefd6b.chunk.js"
+  },
+  {
+    "revision": "902f48547f04d5d4c09f",
+    "url": "/check-your-typing-speed/static/js/runtime-main.a39c5626.js"
+  }
+]);
